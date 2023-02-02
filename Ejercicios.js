00d3f2b3d46897c9c0d323e42af8e2bed6e168fa
unknown <marcosfeijoo97@gmail.com>
2023-02-02 20:02:28 -0300

    first commit

---
+++ Ejercicios.js
@@ -0,0 +1,73 @@
+/*let x="hola, soy var"
+
+console.log(x.length)
+
+function hola(){
+    console.log("hola")
+    
+}*
+//Esto es para contar letras
+
+/*function Texto(cadena=""){
+    if (!cadena){
+        console.warn('por favor ingresa un texto');
+    }
+
+    else {
+        console.info(cadena.substring(0,86))
+    }
+//Esto es para crear una substring
+
+
+}
+Texto()
+Texto("Hola, estoy escribiendo muchas boludeces para saber si me funciona el cadena.susbstring")*/
+
+
+/*let miCadena = 'Hola 1 mundo. Oración número 2.';
+let division = miCadena.split(/(\d)/);
+
+console.log(division);
+
+/*let CadenaARepetir="hola yo soy cosme fulanito"
+
+let CadenaRepetida=CadenaARepetir.repeat(ingresar numero a repetir 25)
+
+console.log(CadenaRepetida)*/
+
+
+
+
+/*consigna:      Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".*/
+
+/*let soyString='Soy un string que tengo que convertir a array';
+let soyElSeparador = soyString.split(' ');
+console.log(soyElSeparador)
+
+let Reverse=soyElSeparador.reverse()
+
+console.log(Reverse)
+
+const reversed = Reverse.join(' ');
+console.log(reversed)*/
+
+/*Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2*/
+
+
+/*const Texto='hola , soy un texto de tipo string,y hola, necesito ver las palabras repetidas necesito hola hola hola hola'
+console.log(Texto)
+Texto===''?alert('Ingresa un texto pelotudo'):console.log('Bien, no era tan dificil')
+
+
+
+const soyArray=Texto.split(' ')
+console.log(soyArray)
+const resultado = soyArray.reduce((prev, cur) => ((prev[cur] = prev[cur] + 1 || 1), prev), {})
+console.log(resultado)
+
+const VuelvoString=soyArray.join('')
+console.log(VuelvoString)*/
+
+/* Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.*/
+
+
